d3d0ccc62a83982ec19108f0ee10d12f03a9b510
Ahmet Avci <ahmetavci.8873@gmail.com>
2022-09-20 22:09:14 +0200

    kitap cikarildi

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import About from "./components/About";
 import Home from "./components/Home";
 import NavbarComponent from "./components/NavbarComponent";
 import {Container} from 'react-bootstrap'
-import { Routes,Route } from "react-router-dom";
+
 
 import Ucuncu from "./components/Ucuncu";
 import Galery from "./components/Galery";
@@ -14,14 +14,7 @@ import Projelerim from "./components/Projelerim";
 import Hacivat from "./components/Hacivat";
 import Nasreddin from "./components/Nasreddin";
 import Oyunlar from "./components/Oyunlar";
-import Kitap from "./components/Kitap";
-import Kitap2 from "./components/Kitap2";
-import Kitap3 from "./components/Kitap3";
-import Kitap4 from './components/Kitap4'
-import Kitap5 from './components/Kitap5'
-import Kitap6 from './components/Kitap6'
-import Kitap7 from './components/Kitap7'
-import KitapButtons from "./components/KitapButtons";
+
 import Son from "./components/Son";
 function App() {
   return (
@@ -43,18 +36,7 @@ function App() {
         <Oyunlar></Oyunlar>
        
         
-        <Routes>
-
-        <Route path="/" element={<Kitap />} />
-        <Route path="/2" element={<Kitap2 />} />
-        <Route path="/3" element={<Kitap3 />} />
-        <Route path="/4" element={<Kitap4/>} />
-        <Route path="/5" element={<Kitap5 />} />
-        <Route path="/6" element={<Kitap6 />} />
-        <Route path="/7" element={<Kitap7 />} />
-        </Routes>
-         
-         <KitapButtons></KitapButtons>
+       
          <Son></Son>
        
        
